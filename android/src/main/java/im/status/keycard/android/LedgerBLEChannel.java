0c4d23a133923cd1de20b357196dc3688e1ae796
Michele Balistreri <michele@bitgamma.com>
2019-06-19 14:35:14 +0300

    finish ble implementation

--- android/src/main/java/im/status/keycard/android/LedgerBLEChannel.java
+++ android/src/main/java/im/status/keycard/android/LedgerBLEChannel.java
@@ -9,22 +9,33 @@ import im.status.keycard.io.LedgerUtil;
 
 import java.io.IOException;
 import java.util.UUID;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class LedgerBLEChannel implements CardChannel {
   final public static UUID LEDGER_UUID = UUID.fromString("13D63400-2C97-0004-0000-4C6564676572");
   final public static UUID LEDGER_REQ_UUID = UUID.fromString("13D63400-2C97-0004-0002-4C6564676572");
   final public static UUID LEDGER_RSP_UUID = UUID.fromString("13D63400-2C97-0004-0001-4C6564676572");
 
-  final private Context context;
+  final private static int BLE_WRITE_FAILED = -1;
+  final private static int BLE_WRITE_STARTED = 0;
+  final private static int BLE_WRITE_FINISHED = 1;
+
+  final private static int BLE_TIMEOUT = 2000;
+
+
   final private BluetoothGatt bluetoothGatt;
   private BluetoothGattCharacteristic reqChar;
   private boolean connected;
   private int mtuSize;
+  private int writeStatus;
+  private LinkedBlockingQueue<byte[]> readQueue;
 
   public LedgerBLEChannel(Context context, BluetoothDevice device) {
-    this.context = context;
     this.connected = false;
     this.mtuSize = 20;
+    this.readQueue = new LinkedBlockingQueue<>();
+    this.writeStatus = BLE_WRITE_FINISHED;
 
     this.bluetoothGatt = device.connectGatt(context, false, new BluetoothGattCallback() {
       @Override
@@ -55,14 +66,19 @@ public class LedgerBLEChannel implements CardChannel {
 
       @Override
       public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+        writeStatus = status == BluetoothGatt.GATT_SUCCESS ? BLE_WRITE_FINISHED : BLE_WRITE_FAILED;
       }
 
       @Override
       public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
         byte[] rsp = characteristic.getValue();
+
         if (rsp[0] == 0x08) {
           mtuSize = rsp[5];
+          return;
         }
+
+        readQueue.offer(rsp);
       }
     });
   }
@@ -72,12 +88,33 @@ public class LedgerBLEChannel implements CardChannel {
     return LedgerUtil.send(cmd, mtuSize, false, new LedgerUtil.Callback() {
       @Override
       public void write(byte[] chunk) throws IOException {
+        writeStatus = BLE_WRITE_STARTED;
+        reqChar.setValue(chunk);
+        bluetoothGatt.writeCharacteristic(reqChar);
 
+        long timeout = 0;
+        while(writeStatus == BLE_WRITE_STARTED || timeout >= BLE_TIMEOUT) {
+          try {
+            Thread.sleep(10);
+            timeout += 10;
+          } catch (InterruptedException e) {
+            throw new IOException("write interrupted");
+          }
+        }
+
+        if (writeStatus != BLE_WRITE_FINISHED) {
+          throw new IOException("write operation failed");
+        }
       }
 
       @Override
       public void read(byte[] chunk) throws IOException {
-
+        try {
+          byte[] data = readQueue.poll(BLE_TIMEOUT, TimeUnit.MILLISECONDS);
+          System.arraycopy(data, 0, chunk, 0, Math.min(data.length, chunk.length));
+        } catch (InterruptedException e) {
+          throw new IOException("read timeout");
+        }
       }
     });
   }
